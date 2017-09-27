86c396aeef882960b610476e61eafc681c59aa1b
radeshshiwlochan <radesh0430@gmail.com>
2017-09-26 22:54:06 -0400

    connects to the database

--- routes/day-routes.js
+++ routes/day-routes.js
@@ -1,5 +1,16 @@
 const express = require('express');
+const connectStr = 'mongodb://localhost:27017';
+let MongoClient = require('mongodb').MongoClient;
 let router = express.Router();
+let db;
+
+MongoClient.connect(connectStr, (err, database)=>{
+    if (err)
+    	throw err;
+    console.log("Successfully connected to the database");
+    db = database;
+});
+
 
 router.get('/day', (req, res) => {
 	let date = req.query.id;
